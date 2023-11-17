ff75c7abaec311824295dfacc65698c20f19237a
GabrielVasquezUT <109549253+Darkseson12@users.noreply.github.com>
2023-11-16 22:26:47 -0500

    Create paquetes.py

---
+++ paquetes.py
@@ -0,0 +1,9 @@
+from Matematicas import Suma
+from Matematicas import Resta
+from Matematicas import Multiplicacion
+from Matematicas import Division
+from Matematicas import Raiz_Cuadrada
+from Matematicas import Potencia
+
+resultado = suma(10, 3)
+print(resultado)  # Imprimirá 8
